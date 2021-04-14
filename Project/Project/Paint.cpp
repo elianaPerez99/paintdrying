54d7d572c6e13b8a15a64a845da73cf408c1716c
eliana perez <eliana.perez@mymail.champlain>
2021-04-14 11:10:32 -0400

    quick changes

--- Project/Project/Paint.cpp
+++ Project/Project/Paint.cpp
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
+
 #include "Paint.h"
-=======
-#include "paint.h"
->>>>>>> 802455722948654921d5a7345e3a45673e9d1b70
+
--- Project/Project/main.cpp
+++ Project/Project/main.cpp
@@ -1,13 +1,7 @@
-<<<<<<< HEAD
+
 #include <string>
 #include <iostream>
 #include "pch.h"
 using namespace std;
 
-=======
-#include "Paint.h"
 
-int main() {
-
-}
->>>>>>> 802455722948654921d5a7345e3a45673e9d1b70
